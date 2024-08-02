712eb835ae0532f24d9fcd44afa77de28e3bdf17
ishitaroy2 <123700704+ishitaroy2@users.noreply.github.com>
2024-08-03 02:00:14 +0530

    Update script1.js

--- script1.js
+++ script1.js
@@ -7,6 +7,8 @@ document.addEventListener("DOMContentLoaded", function () {
   hamburger.addEventListener('click', () => {
     navLeft.classList.toggle('active');
     navRight.classList.toggle('active');
+    // Toggle hamburger icon (optional)
+    hamburger.classList.toggle('active');
   });
 
   // ScrollReveal options and animations
@@ -14,45 +16,53 @@ document.addEventListener("DOMContentLoaded", function () {
     distance: "50px",
     origin: "bottom",
     duration: 1000,
+    easing: 'ease-in-out'
   };
 
-  ScrollReveal().reveal(".container .letter-e", {
+  ScrollReveal().reveal(".letter-e", {
     duration: 1000,
     delay: 1000,
+    ...scrollRevealOption
   });
-  ScrollReveal().reveal(".container img", {
+  ScrollReveal().reveal("img", {
     duration: 1000,
     delay: 1500,
+    ...scrollRevealOption
   });
-  ScrollReveal().reveal(".container .text__left", {
+  ScrollReveal().reveal(".text__left", {
     ...scrollRevealOption,
     origin: "right",
     delay: 2000,
   });
-  ScrollReveal().reveal(".container .text__right", {
+  ScrollReveal().reveal(".text__right", {
     ...scrollRevealOption,
     origin: "left",
     delay: 2000,
   });
-  ScrollReveal().reveal(".container .explore", {
+  ScrollReveal().reveal(".explore", {
     duration: 1000,
     delay: 2500,
+    ...scrollRevealOption
   });
-  ScrollReveal().reveal(".container h5", {
+  ScrollReveal().reveal("h5", {
     duration: 1000,
     interval: 500,
     delay: 3000,
+    ...scrollRevealOption
   });
-  ScrollReveal().reveal(".container .catalog", {
+  ScrollReveal().reveal(".catalog", {
     duration: 1000,
     delay: 5000,
+    ...scrollRevealOption
   });
-  ScrollReveal().reveal(".container .print", {
+  ScrollReveal().reveal(".print", {
     duration: 1000,
     delay: 5500,
+    ...scrollRevealOption
   });
   ScrollReveal().reveal(".footer p", {
     duration: 1000,
     delay: 7000,
+    ...scrollRevealOption
   });
 });
